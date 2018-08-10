639cd78b7c57937411a79a5fb4aa14d0845431d3
theSchnucki <mark.schneider@gmx.net>
2018-08-10 12:46:02 +0200

    Step 1

---
+++ worldofjokes/src/main/java/com/udacity/gradle/builditbigger/worldofjokes/JokeProvider.java
@@ -0,0 +1,12 @@
+package com.udacity.gradle.builditbigger.worldofjokes;
+
+public class JokeProvider {
+    public static String getJoke () {
+        return "This is a very funny joke, indeed. Hahaha!";
+    }
+    public static String getOtherJoke () {
+        return "This is another very funny joke!";
+    }
+
+
+}
