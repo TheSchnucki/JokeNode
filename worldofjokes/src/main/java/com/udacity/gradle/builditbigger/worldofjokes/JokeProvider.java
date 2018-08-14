9962872dac8441861b1c34df73cca8f7be95bb83
theSchnucki <mark.schneider@gmx.net>
2018-08-14 09:35:29 +0200

    JokeProvider returns random joke from array

--- worldofjokes/src/main/java/com/udacity/gradle/builditbigger/worldofjokes/JokeProvider.java
+++ worldofjokes/src/main/java/com/udacity/gradle/builditbigger/worldofjokes/JokeProvider.java
@@ -1,12 +1,18 @@
 package com.udacity.gradle.builditbigger.worldofjokes;
 
+import java.util.Random;
+
 public class JokeProvider {
+    private static String[] jokes = {
+            "Can a kangaroo jump higher than a house? \n Of course, a house doesn't jump at all.",
+            "I have a lot in common with post-first world war Germany. We both went through a great depression in our 20s. Then in our 30s a nice man came along. Great facial hair. I'm hoping for the best. ",
+            "\"Never Apologise! Never Explain!\" Sorry, that's my motto.",
+            "What's the difference between a well dressed man on a bike and a poorly dressed man on a unicycle? \n Attire.",
+            "How do you think the unthinkable? With an itheberg."
+    };
+
     public static String getJoke () {
-        return "This is a very funny joke, indeed. Hahaha!";
+        String returnJoke = jokes[new Random().nextInt(jokes.length)];
+        return returnJoke;
     }
-    public static String getOtherJoke () {
-        return "This is another very funny joke!";
-    }
-
-
 }
